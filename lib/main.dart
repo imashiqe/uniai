5f9e9a6981ba0d2cddf820829fe257d0c9acf9ff
Ashiqur Rahman <imashiqe@gmail.com>
2025-05-13 22:06:06 +0600

    scank bar added

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "My First App",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue,
+            brightness: Brightness.light
+         )
+      ),
       home: HomeActivity(), // Fixed: Call the constructor
     );
   }
 }
 
 class HomeActivity extends StatelessWidget {
+
+  void _showmsg(BuildContext context){
+     ScaffoldMessenger.of(context).showSnackBar(
+         SnackBar(content:  Text("Clicked"))
+     );
+  }
   const HomeActivity({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello"),
+        title: Text("University Ai") ,
+        elevation: 200,
+        actions: [
+          IconButton(onPressed:  (){
+             _showmsg(context);
+          }, icon: Icon(Icons.search_rounded))
+        ],
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       body: Center( // Optional: Center the body text
         child: Text("Body"),
